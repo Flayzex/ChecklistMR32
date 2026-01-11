191699d1904fe492384c79cf1608cef7dfcca9f8
Flayzex <redkiyflayzex@gmail.com>
2026-01-11 13:39:57 +0500

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Checklist
+Checklist for MR-32
---
+++ api/sendAttendance.js
@@ -0,0 +1,35 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const { imageBase64, caption } = req.body;
+
+    const BOT_TOKEN = process.env.BOT_TOKEN;
+    const CHAT_ID = process.env.CHAT_ID;
+
+    if (!BOT_TOKEN || !CHAT_ID) {
+      return res.status(500).json({ error: "Env variables not set" });
+    }
+
+    const telegramUrl = `https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto`;
+
+    const response = await fetch(telegramUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        photo: imageBase64,
+        caption
+      })
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Telegram send failed" });
+  }
+}
